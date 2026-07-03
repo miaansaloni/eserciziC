b99610e1b4fa09e87b9027ee1fda663ae17ba68c
Mia <miansaloni@gmail.com>
2026-07-03 12:49:39 +0200

    make

---
+++ multi-file/make/file2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "file3.h"
+
+void q(char i){
+    printf("%c", f(i));
+}
---
+++ multi-file/make/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "file1.h"
+#include "file2.h"
+
+int main(void){
+    p(1); q(2);
+
+    return 0;
+}
